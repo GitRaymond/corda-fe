b5ff1946bb9521a7cfcab84122e5b69ca334fa59
erkanatesli <erkan.atesli@capgemini.com>
2019-02-08 13:59:29 +0100

    setting up api.

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@ import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import VueRouter from 'vue-router'
 import router from './router';
+import VueResource from 'vue-resource';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
-
-Vue.config.productionTip = false
+Vue.use(VueResource);
+Vue.config.productionTip = false;
 
 new Vue({
     router,
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render: h => h(App),
+}).$mount(('#app'))
