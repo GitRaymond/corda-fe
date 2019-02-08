82ef144aee86a17fef3e019af05a2d1fd1f753c7
Raymond Wensveen <raymond.wensveen@gmail.com>
2019-02-08 12:58:50 +0100

    added vue bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+      <h1>DEEPA</h1>
+      <template v-for="variant in ['primary','secondary','success','outline-success','warning','danger','link']">
+          <div class="col-md-4 pb-2" v-for="size in ['sm','','lg']" :key="`${variant}_${size}`">
+              <b-button :size="size" :variant="variant">
+                  {{variant}} {{size}}
+              </b-button>
+          </div>
+      </template>
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,6 +26,7 @@ export default {
 </script>
 
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
