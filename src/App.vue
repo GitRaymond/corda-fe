c6cfadab529e5890fcae05076bab361433d90557
Raymond Wensveen <raymond.wensveen@gmail.com>
2019-02-08 16:23:36 +0100

    Style updates

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-    <div id="app">
-        <c-header></c-header>
-        <div class="container">
-            <router-view></router-view>
+    <div id="app" >
+        <c-header style="height: 10vh"></c-header>
+        <div class="container " style="height: 90vh;" >
+            <transition name="slide" mode="out-in">
+                <router-view/>
+            </transition>
+
+
         </div>
+
+
 
     </div>
 </template>
@@ -27,6 +33,36 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
+        background-image: url('assets/smart-contracts.jpg');
+    }
+
+    .slide-enter-active {
+        animation: slide-in 200ms ease-out forwards;
+    }
+
+    .slide-leave-active {
+        animation: slide-out 200ms ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+            opacity: 1;
+        }
+        to {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
     }
 </style>
