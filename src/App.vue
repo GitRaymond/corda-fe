d14059ad2c55fff31e7edc1f73661b31a18e77f5
erkanatesli <erkan.atesli@capgemini.com>
2019-02-08 13:33:44 +0100

    Get and post api

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,100 @@
+<!--<template>-->
+  <!--<div id="app">-->
+        <!--<c-header></c-header>-->
+  <!--</div>-->
+<!--</template>-->
+
+<!--<script>-->
+    <!--import Header from './components/Header';-->
+
+<!--export default {-->
+
+
+  <!--name: 'app',-->
+  <!--components: {-->
+      <!--cHeader: Header,-->
+  <!--}-->
+<!--}-->
+<!--</script>-->
+
+<!--<style>-->
+
+<!--#app {-->
+  <!--font-family: 'Avenir', Helvetica, Arial, sans-serif;-->
+   <!-- -webkit-font-smoothing: antialiased;-->
+   <!-- -moz-osx-font-smoothing: grayscale;-->
+  <!--text-align: center;-->
+  <!--color: #2c3e50;-->
+  <!--margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
+
+
 <template>
-  <div id="app">
-        <c-header></c-header>
-  </div>
+    <div class="hello">
+        <h1>Your IP is {{ ip }}</h1>
+        <input type="text" v-model="input.firstname" placeholder="First Name" />
+        <input type="text" v-model="input.lastname" placeholder="Last Name" />
+        <button v-on:click="sendData()">Send</button>
+        <br />
+        <br />
+        <textarea>{{ response }}</textarea>
+    </div>
 </template>
 
 <script>
-    import Header from './components/Header';
-
-export default {
-
-
-  name: 'app',
-  components: {
-      cHeader: Header,
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        data () {
+            return {
+                ip: "",
+                input: {
+                    firstname: "",
+                    lastname: ""
+                },
+                response: ""
+            }
+        },
+        mounted() {
+            this.$http.get("https://httpbin.org/ip").then(result => {
+                this.ip = result.body.origin;
+            }, error => {
+                console.error(error);
+            });
+        },
+        methods: {
+            sendData() {
+                this.$http.post("https://httpbin.org/post", this.input, { headers: { "content-type": "application/json" } }).then(result => {
+                    this.response = result.data.json.firstname + result.data.json.lastname;
+                }, error => {
+                    console.error(error);
+                });
+            }
+        }
+    }
 </script>
 
-<style>
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+    textarea {
+        width: 600px;
+        height: 200px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+import VueResource from 'vue-resource';
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue);
+Vue.use(VueResource);
 Vue.config.productionTip = false
 
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App)
+})
